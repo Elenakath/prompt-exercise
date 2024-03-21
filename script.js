ef6369440230c1a78453ecb6b3618e557264f0ae
elena_e <elenaka123@gmail.com>
2024-03-21 15:28:09 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//var favoriteColor = prompt("What is your favorite color?");
+//console.log(`Your favorite color is ${favoriteColor}.`);
+
+//var name = prompt("What is your name?");
+//var birthYear = prompt("What year were you born? (ex. 1989).");
+//var age = 2024 - birthYear;
+//console.log(`${name} you are ${age} years old.`)
+
+var hourlyWage = prompt("What is your hourly wage? (ex. 15.50)");
+var hoursWorked = prompt("How many hours did you work this week? (ex. 15 for 15 hours)");
+console.log(`You made $${(hourlyWage * hoursWorked).toFixed(2)}`);
+
+
